f48ff9daabeac674957883780361a5d2cc922cd4
Replit user <>
2023-08-15 05:44:52 +0000

    updates

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//EJS is our view engine
+app.set("view engine","ejs");
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  let title = "Home";
+  res.render("pages/index",{"title":title});
+});
+
+app.get('/seattle', (req, res) => {
+  let title = "Seattle Page";
+  res.render("pages/seattle",{"title":title});
+});
+
+app.get('/technology', (req, res) => {
+  let title = "Technology Page";
+  res.render("pages/technology",{"title":title});
+});
+
+app.get('/afghanistan', (req, res) => {
+  let title = "Afghanistan Page";
+  res.render("pages/afghanistan",{"title":title});
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
